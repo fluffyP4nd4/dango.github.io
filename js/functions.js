afb25a2eb54a60c3d5a60e8852f3ef0a6c479ca1
Zera <you@example.com>
2021-02-18 21:44:04 +0530

    finish the structure of the site

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function load(item){
+	if(item==='home')
+		document.getElementById('content').innerHTML='<object tppe = "text/html" data ="home.html"></object>';
+
+	else if(item==='members')
+		document.getElementById('content').innerHTML='<object tppe = "text/html" data ="home.html"></object>';
+
+	else if(item==='projects')
+		document.getElementById('content').innerHTML='<object tppe = "text/html" data ="home.html"></object>';
+
+}
